3d960d914c64a2ba82b08611ab4cb6450c8525fb
Alishba543884 <a38001031@gmail.com>
2026-04-02 14:41:28 +0500

    Shoe store app with Docker

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+shoes = [
+    {"name": "Nike Air", "price": "$120"},
+    {"name": "Adidas Runner", "price": "$100"},
+    {"name": "Puma Sport", "price": "$90"},
+    {"name": "Reebok Classic", "price": "$110"}
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", shoes=shoes)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
